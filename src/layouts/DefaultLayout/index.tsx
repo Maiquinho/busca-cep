a4a1696e721533ef7b4fb25e58293505d83d7d0c
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-02 16:24:55 -0300

    feat: add Footer on DefaultLayout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
 import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
@@ -7,6 +8,7 @@ export function DefaultLayout() {
     <LayoutContainer>
       <Header />
       <Outlet />
+      <Footer />
     </LayoutContainer>
   )
 }
