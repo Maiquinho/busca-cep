3684f72974c32334b0fffb4f5a2b5e852c532af4
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-02 12:18:46 -0300

    feat: add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './global'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-
-      <main>
-        <h1>Hello World</h1>
-      </main>
     </ThemeProvider>
   )
 }
