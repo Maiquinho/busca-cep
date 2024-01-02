69a3c35de2a6b6ff2b7ea9b8b38bf297f2d61577
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-02 15:28:14 -0300

    feat(Container): add max-width on mobile

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -3,4 +3,12 @@ import styled from 'styled-components'
 export const Container = styled.div`
   max-width: 46rem;
   margin: 0 auto;
+
+  @media (max-width: 992px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    max-width: calc(100% - 1rem);
+  }
 `
