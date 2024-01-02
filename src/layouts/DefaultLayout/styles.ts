5e1ef3ab08e528a190ab30bd51edc810ae6ffcb9
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-02 16:25:31 -0300

    feat: set LayoutContainer as flex column

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
-export const LayoutContainer = styled.div``
+export const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
