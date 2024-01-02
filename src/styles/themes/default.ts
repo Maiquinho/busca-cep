cf5079ec13387e3eabd0a8d1ebec20c4c8eca946
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-02 13:06:52 -0300

    feat: add gray-950 on color palette

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -9,6 +9,7 @@ export const defaultTheme = {
   'gray-700': '#29292E',
   'gray-800': '#202024',
   'gray-900': '#121214',
+  'gray-950': '#0D0D0D',
 
   'blue-300': '#0880c7',
   'blue-500': '#06669f',
