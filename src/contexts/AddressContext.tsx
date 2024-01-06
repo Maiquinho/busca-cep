5cd535a17e8617769bf353e243a0619cf0c6b043
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-06 11:59:08 -0300

    feat: convert address into a list, add address localstorage & address localstorage initial state

--- src/contexts/AddressContext.tsx
+++ src/contexts/AddressContext.tsx
@@ -10,7 +10,8 @@ interface Address {
 }
 
 interface AddressContextType {
-  address: Address
+  address: Address[]
+  activeAddress: Address | undefined
   isDisabled: boolean
   isValidResult: boolean
   searchAddress: (zipCode: string) => Promise<void>
@@ -23,41 +24,53 @@ interface AddressProviderProps {
 export const AddressContext = createContext({} as AddressContextType)
 
 export function AddressProvider({ children }: AddressProviderProps) {
-  const [address, setAddress] = useState<Address>({
-    cep: '00000-000',
-    logradouro: '',
-    bairro: '',
-    localidade: '',
-    uf: '',
+  const [address, setAddress] = useState<Address[]>(() => {
+    const storedAddress = localStorage.getItem('@busca-cep:address-state-1.0.0')
+    const lastSearchedAddress = storedAddress ? JSON.parse(storedAddress) : []
+
+    return lastSearchedAddress
   })
+  const [activeAddressId, setActiveAddressId] = useState<string | null>(null)
   const [isDisabled, setIsDisabled] = useState(true)
   const [isValidResult, setIsValidResult] = useState(true)
 
+  const activeAddress = address.find((item) => item.cep === activeAddressId)
+
   async function searchAddress(zipCode?: string) {
-    const response = await api.get(`/${zipCode}/json/`)
+    if (zipCode) {
+      const response = await api.get(`/${zipCode}/json/`)
 
-    if (response.data?.erro === true) {
-      setIsValidResult(false)
+      if (response.data?.erro === true) {
+        setIsValidResult(false)
+        setIsDisabled(false)
+
+        return
+      }
+
+      const fetchedAddress: Address = response?.data
+
+      setAddress((prevState) => [fetchedAddress, ...prevState])
+      setActiveAddressId(fetchedAddress.cep)
       setIsDisabled(false)
-
-      return
+      setIsValidResult(true)
     }
-
-    const fetchedAddress: Address = response?.data
-
-    setAddress(fetchedAddress)
-    setIsDisabled(false)
-    setIsValidResult(true)
   }
 
   useEffect(() => {
     searchAddress()
   }, [])
 
+  useEffect(() => {
+    const addressToJSON = JSON.stringify(address)
+
+    localStorage.setItem('@busca-cep:address-state-1.0.0', addressToJSON)
+  }, [address])
+
   return (
     <AddressContext.Provider
       value={{
         address,
+        activeAddress,
         isDisabled,
         isValidResult,
         searchAddress,
