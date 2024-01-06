49eff93220d0d030d3ebd36366060c1987e4fd43
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-06 12:01:25 -0300

    feat: add History data from AddressContext & add conditional table rendering

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,43 +1,42 @@
+import { useContext } from 'react'
 import { Container } from '../../components/Container/styles'
+import { AddressContext } from '../../contexts/AddressContext'
 import { HistoryTable, HistoryTitle, HistoryWrapper } from './styles'
+import { EmptyHistory } from './components/EmptyHistory'
 
 export function History() {
+  const { address } = useContext(AddressContext)
+
   return (
     <HistoryWrapper>
       <Container>
         <HistoryTitle>
           <h2>Ultimas buscas</h2>
         </HistoryTitle>
-        <HistoryTable>
-          <thead>
-            <tr>
-              <th>CEP</th>
-              <th>Logradouro</th>
-              <th>Município</th>
-              <th>UF</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>07143-222</td>
-              <td>Rua Miguel Fernandes Maldonado</td>
-              <td>Guarulhos</td>
-              <td>SP</td>
-            </tr>
-            <tr>
-              <td>07152-824</td>
-              <td>Rua Araci de Almeida</td>
-              <td>Guarulhos</td>
-              <td>SP</td>
-            </tr>
-            <tr>
-              <td>35160-001</td>
-              <td>Praça José Júlio da Costa</td>
-              <td>Ipatinga</td>
-              <td>MG</td>
-            </tr>
-          </tbody>
-        </HistoryTable>
+
+        {address.length > 0 && (
+          <HistoryTable>
+            <thead>
+              <tr>
+                <th>CEP</th>
+                <th>Logradouro</th>
+                <th>Município</th>
+                <th>UF</th>
+              </tr>
+            </thead>
+            <tbody>
+              {address.map((item) => (
+                <tr key={String(new Date())}>
+                  <td>{item.cep}</td>
+                  <td>{item.logradouro}</td>
+                  <td>{item.localidade}</td>
+                  <td>{item.uf}</td>
+                </tr>
+              ))}
+            </tbody>
+          </HistoryTable>
+        )}
+        {address.length < 1 && <EmptyHistory />}
       </Container>
     </HistoryWrapper>
   )
